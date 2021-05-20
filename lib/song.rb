45a8989b1ebded51dd2d1c6e02c5154d4bb46cf3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-19 22:54:07 -0700

    commit tree 8010eec3601ecc1b4e6c8b80f64322449ae7e67a

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        genre_histogram = {}
+        @@genres.each do |genre|
+            genre_histogram[genre] ? genre_histogram[genre] += 1 : genre_histogram[genre] = 1
+        end
+        genre_histogram
+    end
+
+    def self.artist_count
+        artist_histogram = {}
+        @@artists.each do |artist|
+            artist_histogram[artist] ? artist_histogram[artist] += 1 : artist_histogram[artist] = 1
+        end
+        artist_histogram
+    end
+
+    attr_accessor :name, :artist, :genre
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+end
